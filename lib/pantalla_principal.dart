3912bcebf9a1733f2cdd7b8b06c17620ee36b4cb
DanielGiron20 <girondaniel388@gmail.com>
2024-09-10 11:08:57 -0600

    despliegue de cards con info de los productos

--- lib/pantalla_principal.dart
+++ lib/pantalla_principal.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; 
 import 'package:pumitas_emprendedores/BaseDeDatos/db_helper.dart';
 import 'package:pumitas_emprendedores/BaseDeDatos/usuario.dart';
 import 'package:pumitas_emprendedores/rutas.dart';
-import 'package:pumitas_emprendedores/wigets/product_card.dart';
 
 class PantallaPrincipal extends StatefulWidget {
   const PantallaPrincipal({super.key});
@@ -32,23 +32,20 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
   }
 
   Future<void> _loadProducts() async {
-    //simulacion de la carga de porductos
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference productsCollection = firestore.collection('products');
+
+    QuerySnapshot snapshot = await productsCollection.get();
     setState(() {
-      _products = [
-        {
-          'nombre': 'Goku ssj 3 ultra instinto',
-          'descripcion': 'Si',
-          'imagenUrl': 'https://i.ytimg.com/vi/fpPQq3U8epM/maxresdefault.jpg',
-          'precio': 9.99,
-        },
-        {
-          'nombre': 'Goku no se que transformacion',
-          'descripcion': 'No',
-          'imagenUrl':
-              'https://images.wallpapersden.com/image/download/goku-ultra-instinct-hd-digital-art_bmZmZ2aUmZqaraWkpJRnamtorWZmbmY.jpg',
-          'precio': 19.99,
-        },
-      ];
+      _products = snapshot.docs.map((doc) {
+        return {
+          'name': doc['name'],
+          'description': doc['description'],
+          'image': doc['image'],
+          'price': doc['price'],
+          'category': doc['category'],
+        };
+      }).toList();
     });
   }
 
@@ -81,7 +78,7 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
                   ],
                 )
               : IconButton(
-                  icon: Icon(Icons.login),
+                  icon: const Icon(Icons.login),
                   onPressed: () {
                     Navigator.pushNamed(context, MyRoutes.Login.name);
                   },
@@ -99,17 +96,123 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
       );
     }
 
-    return ListView.builder(
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1, //Aca es si queres que se vea mas de una columna de productos
+        childAspectRatio: 1.1, // y aca el alto de los cards, no se poruue aca menos es mas jajaja
+      ),
       itemCount: _products.length,
       itemBuilder: (context, index) {
         final product = _products[index];
         return ProductCard(
-          nombre: product['nombre'],
-          descripcion: product['descripcion'],
-          imagenUrl: product['imagenUrl'],
-          precio: product['precio'],
+          nombre: product['name'],
+          descripcion: product['description'],
+          imagenUrl: product['image'],
+          precio: product['price'],
+          categoria: product['category'], 
         );
       },
+    );
+  }
+}
+
+//clase del card
+class ProductCard extends StatelessWidget {
+  final String nombre;
+  final String descripcion;
+  final String imagenUrl;
+  final double precio;
+  final String categoria;
+
+  const ProductCard({
+    required this.nombre,
+    required this.descripcion,
+    required this.imagenUrl,
+    required this.precio,
+    required this.categoria,
+    Key? key,
+  }) : super(key: key);
+
+
+// No te miento, esta apariencia se la pedi a chatsitoGPT
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10), 
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15), 
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, 
+        children: [
+          
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+            ),
+            child: Image.network(
+              imagenUrl,
+              height: 200, 
+              width: double.infinity,
+              fit: BoxFit.cover, 
+            ),
+          ),
+          const SizedBox(height: 10),
+        
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Text(
+              nombre,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold, 
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+        
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Precio: \$$precio',
+                  style: const TextStyle(
+                    fontSize: 14, 
+                    color: Colors.grey,
+                  ),
+                ),
+                Text(
+                  'Categoría: $categoria',
+                  style: const TextStyle(
+                    fontSize: 14, 
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: ElevatedButton(// boton donde desplegaremos toda la info del producto y vendedor
+              onPressed: () {
+                
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('Ver más'),
+            ),
+          ),
+          const SizedBox(height: 10), 
+        ],
+      ),
     );
   }
 }
